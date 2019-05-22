fe530ae4f0b3bf86b1ba1ed158d5a567390c19a9
Irene Rosell <irenerosellg@gmail.com>
2019-05-22 18:10:00 +0200

    Creamos un endponit de creación de usuarios

--- users/api.py
+++ users/api.py
@@ -1,21 +1,38 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserListSerializer, WriteUserSerializer
 
 
 class UsersAPI(APIView):
 
+
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        serializer = UserListSerializer(users, many=True)
 
         return Response(serializer.data)
 
 
+
+    def post(self, request):
+        serializer = WriteUserSerializer(data=request.POST)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            user_serializer = UserSerializer(new_user)
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 class UserDetailAPI(APIView):
+
 
     def get(self, request, pk):
         user = get_object_or_404(User, pk=pk)
--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,49 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-class UserSerializer(serializers.Serializer):
 
-    id = serializers.IntegerField()
+class UserListSerializer(serializers.Serializer):
+
+    id = serializers.ReadOnlyField()
     username = serializers.CharField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
 
-    def create(self, validated_data):
-        pass
 
-    def update(self, instance, validated_data):
-        pass
+
+class UserSerializer(UserListSerializer):
+
+    email = serializers.CharField()
+    date_joined = serializers.ReadOnlyField()
+    last_login = serializers.ReadOnlyField()
+
+
+class WriteUserSerializer(UserSerializer):
+
+    password = serializers.CharField()
+    confirm_password = serializers.CharField()
+
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise ValidationError('The username {0} is already used'.format(value))
+        return value
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        confirm_password = attrs.get('confirm_password')
+        if password != confirm_password:
+            raise ValidationError('Passwords do not match')
+        return attrs
+
+
+    def create(self, validated_data):
+        user = User()
+        user.first_name = validated_data.get('first_name')
+        user.last_name = validated_data.get('last_name')
+        user.username = validated_data.get('username')
+        user.set_password(validated_data.get('password'))
+        user.email = validated_data.get('email')
+        user.save()
+        return user
